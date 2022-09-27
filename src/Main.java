c2007664f854f039e3beebbb507eaa68a6421919
Yuriy Sviridov <crip.shooter@gmail.com>
2022-09-27 15:08:21 +0300

    add correct queue.poll();

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Queue<Person> queue = new LinkedList<>(generateClients());
         while (!queue.isEmpty()) {
-            Person person = queue.peek();
+            Person person = queue.poll();
             person.ticketCount--;
-            queue.poll();
             System.out.println(person + " прокатился на аттракционе");
             if (person.ticketCount > 0) {
                 queue.offer(person);
@@ -18,11 +17,11 @@ public class Main {
     }
     static List<Person> generateClients() {
         List<Person> personList = new LinkedList<>();
-        personList.add(new Person("Andrew", "Garfield", 10));
+        personList.add(new Person("Andrew", "Garfield", 6));
         personList.add(new Person("Jack", "Daniels", 5));
-        personList.add(new Person("Jim", "Beam", 7));
+        personList.add(new Person("Jim", "Beam", 4));
         personList.add(new Person("Elon", "Musk", 3));
-        personList.add(new Person("Michael", "Keaton", 12));
+        personList.add(new Person("Michael", "Keaton", 1));
         return personList;
     }
 }
